4f29a7f67e8e876350714d6729c6d5df42d261bc
Marco <marcoantoniotd@gmail.com>
2021-10-26 16:42:49 +0100

    Get data from url successfully

--- Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/GameActivityViewModel.kt
+++ Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/GameActivityViewModel.kt
@@ -1,32 +1,34 @@
 package pt.isel.pdm.chess4android
 
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.ViewModel
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class GameActivityViewModel: ViewModel() {
     companion object {
         val service = Retrofit.Builder()
-            .baseUrl("https://lichess.org/api/puzzle/daily/")
+            .baseUrl("https://lichess.org/api/puzzle/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ChessRoyaleService::class.java)
-
     }
 
-    fun getPuzzleOfDay(completion: (String) -> Unit) {
+    fun getPuzzleOfDay(completion: (Object) -> Unit) {
         //Let's make the request
         service.getPuzzle().enqueue(object: Callback<Test> {
             override fun onResponse(call: Call<Test>, response: Response<Test>) {
-                completion(response.body()?.game ?: "")
+                response.body()?.game?.let { completion(it) }
             }
 
             override fun onFailure(call: Call<Test>, t: Throwable) {
-                Log.e("APP_TAG", "onFailure", t)
+                Log.e("APP", "onFailure", t)
             }
 
         })
