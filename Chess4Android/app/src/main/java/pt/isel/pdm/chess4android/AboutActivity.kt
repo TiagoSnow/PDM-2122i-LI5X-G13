4617edfd94849a212e370f0d0af7eca915e07649
Marco <marcoantoniotd@gmail.com>
2021-11-21 17:44:00 +0000

    Fix about Activity

--- Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/AboutActivity.kt
+++ Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/AboutActivity.kt
@@ -1,13 +1,15 @@
 package pt.isel.pdm.chess4android
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import android.widget.PopupMenu
-import android.widget.Toast
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AlertDialog
 import pt.isel.pdm.chess4android.databinding.ActivityAboutBinding
-import pt.isel.pdm.chess4android.databinding.ActivityMainBinding
+
+private const val LINCHESS_URL = "https://lichess.org/api"
 
 class AboutActivity : AppCompatActivity() {
 
@@ -15,37 +17,48 @@ class AboutActivity : AppCompatActivity() {
         ActivityAboutBinding.inflate(layoutInflater)
     }
 
+    lateinit var dialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.authorsButton.setOnClickListener {
-            showPopup(binding.authorsButton)
+            showPopupAuthors(R.layout.authors_popup)
         }
+
+        binding.creditsButton!!.setOnClickListener {
+            showPopupCredits(R.layout.credits_popup)
+        }
+
     }
 
-    private fun showPopup(view: View) {
-        val popup = PopupMenu(this, view)
-        popup.inflate(R.menu.menu)
+    private fun showPopupAuthors(popupViewId: Int) {
+        val alertDialog = AlertDialog.Builder(this)
+        val popup = layoutInflater.inflate(popupViewId, null)
 
-        popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+        alertDialog.setView(popup)
+        dialog = alertDialog.create()
+        dialog.show()
+    }
 
-            when (item!!.itemId) {
-                R.id.header1 -> {
-                    Toast.makeText(this@AboutActivity, item.title, Toast.LENGTH_SHORT).show()
-                }
-                R.id.header2 -> {
-                    Toast.makeText(this@AboutActivity, item.title, Toast.LENGTH_SHORT).show()
-                }
-                R.id.header3 -> {
-                    Toast.makeText(this@AboutActivity, item.title, Toast.LENGTH_SHORT).show()
-                }
+    private fun showPopupCredits(popupViewId: Int) {
+        val alertDialog = AlertDialog.Builder(this)
+        val popup = layoutInflater.inflate(popupViewId, null)
+
+        var apiText = popup.findViewById<TextView>(R.id.api_text)
+
+        alertDialog.setView(popup)
+        dialog = alertDialog.create()
+        dialog.show()
+
+        apiText.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(LINCHESS_URL)).apply {      //abre uma nova pagina web criando uma nova task
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
             }
+            startActivity(intent)
+        }
 
-            true
-        })
-
-        popup.show()
     }
 
 }
